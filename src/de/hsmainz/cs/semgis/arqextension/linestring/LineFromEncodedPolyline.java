dce71cd757189b22aa10ef12a4af707b472f8b99
Your Name <you@example.com>
2019-04-27 16:01:46 +0200

    Added LineFromMultiPoint

---
+++ src/de/hsmainz/cs/semgis/arqextension/linestring/LineFromEncodedPolyline.java
@@ -0,0 +1,14 @@
+package de.hsmainz.cs.semgis.arqextension.linestring;
+
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase1;
+
+public class LineFromEncodedPolyline extends FunctionBase1 {
+
+	@Override
+	public NodeValue exec(NodeValue v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
