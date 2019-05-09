89a7f8700383199492915a452121082928e3967f
situx <timo.homburg@gmx.de>
2019-05-09 20:38:19 +0200

    IsClosedTest

--- src/main/de/hsmainz/cs/semgis/arqextension/geometry/GeomFromGeoHash.java
+++ src/main/de/hsmainz/cs/semgis/arqextension/geometry/GeomFromGeoHash.java
@@ -2,11 +2,6 @@ package de.hsmainz.cs.semgis.arqextension.geometry;
 
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
-import org.locationtech.jts.geom.Geometry;
-import org.wololo.jts2geojson.GeoJSONReader;
-
-import io.github.galbiston.geosparql_jena.implementation.GeometryWrapper;
-import io.github.galbiston.geosparql_jena.implementation.datatype.WKTDatatype;
 /**
  * Return a geometry from a GeoHash string.
  *
